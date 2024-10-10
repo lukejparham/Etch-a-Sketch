b0a63fb531cb061bfe4b1226c2b1e3a6026a53f0
Luke Parham <lukejparham@gmail.com>
2024-10-10 12:01:04 -0400

    grid layout created in game.js/styles.css

---
+++ game.js
@@ -0,0 +1,22 @@
+const sizeOfGrid = 16;
+const container = document.querySelector(".container");
+
+const createGrid = (amtOfGrids) => {
+  for (let i = 0; i < amtOfGrids; i++) {
+    const row = document.createElement("div");
+    row.classList.add("grid-row");
+
+    for (let j = 0; j < amtOfGrids; j++) {
+      const widthAndHeight = 960 / sizeOfGrid;
+      const gridBox = document.createElement("div");
+      gridBox.classList.add("grid-box");
+      gridBox.style.width = `${widthAndHeight}px`;
+      gridBox.style.height = `${widthAndHeight}px`;
+      row.appendChild(gridBox);
+    }
+
+    container.appendChild(row);
+  }
+};
+
+createGrid(sizeOfGrid);
